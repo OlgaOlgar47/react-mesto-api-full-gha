39c342332473c2cc2eddf2e7bfb9ab94a4cf37eb
Olga Averianova <OlgaOlgar47@yandex.ru>
2023-05-16 01:01:17 -0300

    aa

--- backend/app.js
+++ backend/app.js
@@ -18,6 +18,10 @@ const app = express();
 app.use(
   cors({
     origin: '*',
+    methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
+    allowedHeaders: ['Content-type', 'Authorization'],
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
   })
 );
 
