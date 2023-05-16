8c223e176d730e5b6c62b6073246800747701c4d
Olga Averianova <OlgaOlgar47@yandex.ru>
2023-05-16 11:33:22 -0300

    cors

--- backend/app.js
+++ backend/app.js
@@ -17,9 +17,10 @@ mongoose.connect(DATABASE_URL);
 const app = express();
 app.use(
   cors({
-    origin: '*',
+    origin: 'https://mestocean.nomoredomains.monster',
     methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
     allowedHeaders: ['Content-type', 'Authorization'],
+    credentials: true,
     preflightContinue: false,
     optionsSuccessStatus: 204,
   })
