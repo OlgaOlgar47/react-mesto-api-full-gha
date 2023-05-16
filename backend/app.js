c8fba5b75ef7c94dab4ee522d84d7be6e05926e6
Olga Averianova <OlgaOlgar47@yandex.ru>
2023-05-16 00:56:08 -0300

    aa

--- backend/app.js
+++ backend/app.js
@@ -20,6 +20,18 @@ app.use(
     origin: '*',
   })
 );
+
+app.use((req, res, next) => {
+  res.setHeader(
+    'Access-Control-Allow-Origin',
+    'https://mestocean.nomoredomains.monster'
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  next();
+});
+
 app.use(cookieParser());
 
 require('dotenv').config();
